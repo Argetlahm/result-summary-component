83b9aa5ccca066b103886b00b603eefa892bfd9a
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2023-09-13 23:19:07 +0700

    update some files d:13092023 g:fetch data from json properly

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var arrData = {};
+var arrData = [];
 
 //must use live server to avoid CORS error. Use Live Server plugin in VS code. (ctrl + shift + p => type live server)
 function fetchData(url) {
     fetch(url)
         .then((response) => response.json())
-        .then((json) => arrData.data = json)
+        .then((json) => arrData.push(json))
 }
 
 fetchData("./data.json")
-console.log(arrData)
+// console.log(arrData)
 
-const scoreElem = document.getElementsByClassName("summary-score")
+const scoreElem = document.getElementsByClassName("summary-title")
 for (let i = 0; i < scoreElem.length; i++) {
-    // const element = scoreElem[i];
-    // console.log(scoreElem)
+    const element = scoreElem[i];
+    // scoreElem[i].innerHTML = arrData.data[0].category
+    console.log(arrData)
 }
